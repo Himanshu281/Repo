46d8f0cf16e504cd421124a5dc96d50cf9b166db
Himanshu <himanshudwivedi714@gmail.com>
2022-04-07 15:10:29 +0530

    Updates

---
+++ precache-manifest.1e241fab6ecc545caa79b4fba7150700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2624ad082bfff9bb7179",
+    "url": "/Repo/static/css/main.5a66cf3e.chunk.css"
+  },
+  {
+    "revision": "2624ad082bfff9bb7179",
+    "url": "/Repo/static/js/main.2624ad08.chunk.js"
+  },
+  {
+    "revision": "b30741ea70ee088bcae7",
+    "url": "/Repo/static/js/1.b30741ea.chunk.js"
+  },
+  {
+    "revision": "7f9d2c53be518829638f",
+    "url": "/Repo/static/js/2.7f9d2c53.chunk.js"
+  },
+  {
+    "revision": "be23a1b1a798c1eeb479",
+    "url": "/Repo/static/js/runtime~main.be23a1b1.js"
+  },
+  {
+    "revision": "0e4fea7a08f7a6515445307d059955f0",
+    "url": "/Repo/index.html"
+  }
+];
